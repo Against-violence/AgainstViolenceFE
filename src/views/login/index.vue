e4382e1b882e515be891d7e6dd70fd737cb4f8a5
88384490 <31804222+88384490@users.noreply.github.com>
2019-11-02 19:19:37 +0800

    修改登录错误的bug

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -140,14 +140,13 @@ export default {
     handleLogin() {
       this.$refs.loginForm.validate(valid => {
         if (valid) {
-          this.router.push({ path: '/' })
-          // this.loading = true
-          // this.$store.dispatch('user/login', this.loginForm).then(() => {
-          //   this.$router.push({ path: this.redirect || '/' })
-          //   this.loading = false
-          // }).catch(() => {
-          //   this.loading = false
-          // })
+          this.loading = true
+          this.$store.dispatch('user/login', this.loginForm).then(() => {
+            this.$router.push({ path: this.redirect || '/' })
+            this.loading = false
+          }).catch(() => {
+            this.loading = false
+          })
         } else {
           console.log('error submit!!')
           return false
