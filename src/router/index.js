c199d5ae3ece09053cabbe51561bc89796016870
Luzy <felixxg@foxmail.com>
2019-11-04 00:24:52 +0800

    noprogress && 设置中心

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import NProgress from 'nprogress' // Progress 进度条
+import 'nprogress/nprogress.css' // Progress 进度条样式
 import Register from '@/views/Register/Register.vue'
 import Login from '@/views/Login/Login.vue'
 import Main from '@/views/Main/Main.vue'
@@ -10,62 +12,65 @@ Vue.use(Router)
 
 const router = new Router({
   routes: [{
-      path: '/login',
-      name: 'login',
-      component: Login,
-      meta: {
-        isPublic: true
-      }
+    path: '/login',
+    name: 'login',
+    component: Login,
+    meta: {
+      isPublic: true
+    }
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register,
+    meta: {
+      isPublic: true
+    }
+  },
+  {
+    path: '/',
+    name: 'main',
+    component: Main,
+    meta: {
+      requireAuth: true
     },
-    {
-      path: '/register',
-      name: 'register',
-      component: Register,
-      meta: {
-        isPublic: true
-      }
-    },
-    {
-      path: '/',
-      name: 'main',
-      component: Main,
+    // redirect: '/home',
+    children: [{
+      path: '/home',
+      name: 'home',
+      component: Home,
       meta: {
         requireAuth: true
-      },
-      // redirect: '/home',
-      children: [{
-          path: '/home',
-          name: 'home',
-          component: Home,
-          meta: {
-            requireAuth: true
-          }
-        },
-        {
-          path: '/hc/list',
-          name: 'hc',
-          component: HelpingCenter,
-          meta: {
-            requireAuth: true
-          }
-        },
-        {
-          path: '/sc/home',
-          name: 'scHome',
-          component: SettingCenter,
-          meta: {
-            requireAuth: true
-          }
-        }
-      ]
+      }
     },
+    {
+      path: '/hc/list',
+      name: 'hc',
+      component: HelpingCenter,
+      meta: {
+        requireAuth: true
+      }
+    },
+    {
+      path: '/sc/home',
+      name: 'scHome',
+      component: SettingCenter,
+      meta: {
+        requireAuth: true
+      }
+    }
+    ]
+  },
 
   ]
 })
-// router.beforeEach((to, from ,next) => {
-//   if (!to.meta.isPublic && !localStorage.token) {
-//     return next('/login')
-//   }
-//   next()
-// })
+router.beforeEach((to, from, next) => {
+  if (to.path !== from.path) {
+    NProgress.start();
+  }
+  next();
+});
+router.afterEach(route => {
+  NProgress.done();
+})
 export default router
--- src/views/SettingCenter/SettingCenter.vue
+++ src/views/SettingCenter/SettingCenter.vue
@@ -5,7 +5,8 @@
         <el-menu-item>
           <template slot="title"><i class="el-icon-user-solid"></i>我的主页</template>
         </el-menu-item>
-        <el-menu-item index="/hc">
+        <!-- index="/hc" -->
+        <el-menu-item>
           <template slot="title"><i class="el-icon-chat-round"></i>我的发帖</template>
         </el-menu-item>
         <el-menu-item>
@@ -28,23 +29,93 @@
           </el-upload>
         </el-tab-pane>
         <el-tab-pane label="修改头像" name="second">
+          <el-tab-pane label="基本资料" name="first">
+            <el-upload class="upload-demo" action="https://jsonplaceholder.typicode.com/posts/" :on-preview="handlePreview" :on-remove="handleRemove" :file-list="fileList" list-type="picture">
+              <el-button size="small" type="primary">点击上传</el-button>
+              <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+            </el-upload>
+          </el-tab-pane>
         </el-tab-pane>
         <el-tab-pane label="修改密码" name="third">
-          <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+          <el-form :model="passwordFrom" status-icon :rules="passwordRules" ref="passwordFrom" label-width="100px">
             <el-form-item label="密码" prop="pass">
-              <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+              <el-input type="password" v-model="passwordFrom.pass" autocomplete="off"></el-input>
             </el-form-item>
             <el-form-item label="确认密码" prop="checkPass">
-              <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+              <el-input type="password" v-model="passwordFrom.checkPass" autocomplete="off"></el-input>
             </el-form-item>
             <el-form-item>
-              <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-              <el-button @click="resetForm('ruleForm')">重置</el-button>
+              <el-button type="primary" @click="submitForm('passwordFrom')">提交</el-button>
+              <el-button @click="resetForm('passwordFrom')">重置</el-button>
             </el-form-item>
           </el-form>
         </el-tab-pane>
-        <el-tab-pane label="邮箱认证" name="fourth">
-
+        <el-tab-pane label="实名认证" name="fourth">
+          <el-form label-position="left" :model="idVerifiedFrom" status-icon ref="idVerifiedFrom" label-width="100px">
+            <el-form-item label="姓名" prop="name">
+              <el-input v-model="idVerifiedFrom.name" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="身份证号码" prop="idCardNumber">
+              <el-input v-model="idVerifiedFrom.idCardNumber" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-form>
+          <div class="up-button">
+            <div class="right-distance20">
+              <el-button type="primary">上传<i class="el-icon-upload el-icon--right "></i></el-button>
+            </div>
+            <span>建议尺寸200*200，支持jpg、png、gif，最大不能超过500KB</span>
+          </div>
+          <div class="idcard-warpper top-distance20">
+            <div class="left-idcard">
+              <span class="bottom-distance20">请输入学生证正面或身份证正面</span>
+              <el-upload action="#" list-type="picture-card" :auto-upload="false">
+                <i slot="default" class="el-icon-plus"></i>
+                <div slot="file" slot-scope="{file}">
+                  <img class="el-upload-list__item-thumbnail" :src="file.url" alt="">
+                  <span class="el-upload-list__item-actions">
+                    <span class="el-upload-list__item-preview" @click="handleFontPictureCardPreview(file)">
+                      <i class="el-icon-zoom-in"></i>
+                    </span>
+                    <span v-if="!fontDisabled" class="el-upload-list__item-delete" @click="handleDownload(file)">
+                      <i class="el-icon-download"></i>
+                    </span>
+                    <span v-if="!fontDisabled" class="el-upload-list__item-delete" @click="handleRemove(file)">
+                      <i class="el-icon-delete"></i>
+                    </span>
+                  </span>
+                </div>
+              </el-upload>
+              <el-dialog :visible.sync="fontDialogVisible">
+                <img width="100%" :src="fontDialogImageUrl" alt="">
+              </el-dialog>
+            </div>
+            <div class="right-idcard">
+              <span class="bottom-distance20">请输入学生证反面或身份证反面</span>
+              <el-upload action="#" list-type="picture-card" :auto-upload="false" :limit="1">
+                <i slot="default" class="el-icon-plus"></i>
+                <div slot="file" slot-scope="{file}">
+                  <img class="el-upload-list__item-thumbnail" :src="file.url" alt="">
+                  <span class="el-upload-list__item-actions">
+                    <span class="el-upload-list__item-preview" @click="handleBackPictureCardPreview(file)">
+                      <i class="el-icon-zoom-in"></i>
+                    </span>
+                    <span v-if="!backDisabled" class="el-upload-list__item-delete" @click="handleDownload(file)">
+                      <i class="el-icon-download"></i>
+                    </span>
+                    <span v-if="!backDisabled" class="el-upload-list__item-delete" @click="handleRemove(file)">
+                      <i class="el-icon-delete"></i>
+                    </span>
+                  </span>
+                </div>
+              </el-upload>
+              <el-dialog :visible.sync="backDialogVisible">
+                <img width="100%" :src="backDialogImageUrl" alt="">
+              </el-dialog>
+            </div>
+          </div>
+          <div class="top-distance20">
+            <el-button type="primary">确认修改</el-button>
+          </div>
         </el-tab-pane>
       </el-tabs>
     </el-main>
@@ -62,6 +133,39 @@
 .el-aside {
     color: #333;
 }
+.up-button {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+.right-distance20 {
+    margin-right: 20px;
+}
+.idcard-warpper {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
+.left-idcard {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex: 1;
+    margin-right: 20px;
+}
+.right-idcard {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex: 1;
+}
+.bottom-distance20 {
+    margin-bottom: 20px;
+}
+.top-distance20 {
+    margin-top: 40px;
+}
 </style>
 
 <script>
@@ -71,8 +175,8 @@ export default {
             if (value === "") {
                 callback(new Error("请输入密码"));
             } else {
-                if (this.ruleForm.checkPass !== "") {
-                    this.$refs.ruleForm.validateField("checkPass");
+                if (this.passwordFrom.checkPass !== "") {
+                    this.$refs.passwordFrom.validateField("checkPass");
                 }
                 callback();
             }
@@ -80,7 +184,7 @@ export default {
         let validatePass2 = (rule, value, callback) => {
             if (value === "") {
                 callback(new Error("请再次输入密码"));
-            } else if (value !== this.ruleForm.pass) {
+            } else if (value !== this.passwordFrom.pass) {
                 callback(new Error("两次输入密码不一致!"));
             } else {
                 callback();
@@ -101,14 +205,44 @@ export default {
                         "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100"
                 }
             ],
-            ruleForm: {
+            passwordFrom: {
                 pass: "",
                 checkPass: ""
             },
-            rules: {
+            idVerifiedFrom: {
+                name: "",
+                idCardNumber: ""
+            },
+            idVerifiedRules: {
+                name: [
+                    {
+                        required: true,
+                        message: "请输入姓名",
+                        trigger: "blur"
+                    }
+                ],
+                idCardNumber: [
+                    {
+                        required: true,
+                        message: "请输入身份证号码",
+                        trigger: "blur"
+                    },
+                    {
+                        pattern: /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/,
+                        message: "你的身份证格式不正确"
+                    }
+                ]
+            },
+            passwordRules: {
                 pass: [{ validator: validatePass, trigger: "blur" }],
                 checkPass: [{ validator: validatePass2, trigger: "blur" }]
-            }
+            },
+            fontDialogImageUrl: "",
+            fontDialogVisible: false,
+            fontDisabled: false,
+            backDialogImageUrl: "",
+            backDialogVisible: false,
+            backDisabled: false
         };
     },
     methods: {
@@ -131,6 +265,14 @@ export default {
         },
         handlePreview(file) {
             console.log(file);
+        },
+        handleFontPictureCardPreview(file) {
+            this.fontDialogImageUrl = file.url;
+            this.fontDialogVisible = true;
+        },
+        handleBackPictureCardPreview(file) {
+            this.backDialogImageUrl = file.url;
+            this.backDialogVisible = true;
         }
     }
 };
